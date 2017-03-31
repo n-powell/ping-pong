be6e8c8b9461e57ef58faef88b2c361fb5d8d50d
Nick Powell <nickpowells@gmail.com>
2017-03-31 16:53:59 -0700

    add finishes to ghpages.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business
-var everyThird = "ping" //these are unnecesary but I wanted some business logic variables
-var everyFifth = "pong" //these are unnecesary but I wanted some business logic variables
-var everyFifteenth = "ping-pong" //these are unnecesary but I wanted some business logic variables
+var everyThird = "ping" //these are unnecessary but I wanted some business logic variables
+var everyFifth = "pong" //these are unnecessary but I wanted some business logic variables
+var everyFifteenth = "ping-pong" //these are unnecessary but I wanted some business logic variables
 
 
 //User Logic
@@ -13,17 +13,17 @@ $(function() {
     var userInput = $("#userInput").val();
     for (var index = 1; index <= userInput; index += 1) {
       if (index % 15 === 0) {
-        $("#ifVictory").text("The ball got stuck under the couch, try again")
+        $("#ifVictory").text("The ball got stuck under the couch, try again.")
         $("#output").append("<br><li>" + everyFifteenth + "</li><br>");
       }else if (index % 5 === 0) {
-        $("#ifVictory").text("Of course you have just lost to Derek Zoolander at Ping-Pong")
+        $("#ifVictory").text("Of course you have just lost to Derek Zoolander at Ping-Pong.")
         $("#output").append("<li>" + everyFifth + "</li>")
       }else if (index % 3 === 0) {
-        $("#ifVictory").text("You have just beaten Derek Zoolander at Ping-Pong")
+        $("#ifVictory").text("It is truly unbelievable. You Won. You have just ruined Derek Zoolanders entire modeling career.")
         $("#output").append("<li>" + everyThird + "</li>");
       }else
-        $("#ifVictory").text("A lot of hard work, but still no winner, play again")
-      $("#output").append("<li>" + index + "</li>");
+        $("#ifVictory").text("A lot of hard work, but still no winner, play again.")
+        $("#output").append("<li>" + index + "</li>");
     }
   });
 });
